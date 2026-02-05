a8842fa13d5f445829b2d048f9acc42af0b2a6ea
Juuso Toivanen <juuso.v.toivanen@tuni.fi>
2026-02-05 10:08:32 +0200

    First version of schema done

--- backend/src/config/schema.sql
+++ backend/src/config/schema.sql
@@ -5,17 +5,107 @@ CREATE TABLE medicine (
     image_path TEXT,
     description TEXT,
 
-    form_id INTEGER FOREIGN KEY,
-    age_group_id INTEGER FOREIGN KEY,
-    dosage_id INTEGER FOREIGN KEY,
-    inhalation_requirement_id FOREIGN KEY,
-    inhaler_id FOREIGN KEY,
-    drug_purpose_id FOREIGN KEY,
-    drug_class_id FOREIGN KEY, --mahdollisesti yhdistetty lääkkeeseen vain
-    active_ingredient_id FOREIGN KEY,
-    color_id FOREIGN KEY
+
+    age_group_id INTEGER,
+    dosage_id INTEGER,
+    inhalation_requirement_id INTEGER,
+    drug_purpose_id INTEGER,
+    color_id INTEGER,
+
+    FOREIGN KEY (age_group_id) REFERENCES age_group(id),
+    FOREIGN KEY (dosage_id) REFERENCES dosage(id),
+    FOREIGN KEY (inhalation_requirement_id) REFERENCES inhalation_requirement(id),
+    FOREIGN KEY (drug_purpose_id) REFERENCES drug_purpose(id),
+    FOREIGN KEY (color_id) REFERENCES color(id)
 
 );
+-- ONE TO MANY vvvv-----------------------------------------------------
+
+CREATE TABLE age_group (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE dosage (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE inhalation_requirement (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE drug_purpose (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+); 
+
+CREATE TABLE color (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+-- ---------------------------------------------------------------------
+
+-- MANY TO MANY vvvv----------------------------------------------------
+
+-- Lääkemuoto
+CREATE TABLE drug_form (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+
+CREATE TABLE medicine_drug_form (
+    medicine_id INTEGER NOT NULL,
+    drug_form_id INTEGER NOT NULL,
+
+    PRIMARY KEY (medicine_id, drug_form_id),
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (drug_form_id) REFERENCES drug_form(id) ON DELETE RESTRICT
+);
+
+-- Inhalaattorit
+CREATE TABLE inhaler (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE medicine_inhaler (
+    medicine_id INTEGER NOT NULL,
+    inhaler_id INTEGER NOT NULL,
+
+    PRIMARY KEY (medicine_id, inhaler_id),
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (inhaler_id) REFERENCES inhaler(id) ON DELETE RESTRICT
+);
+
+
+--Lääkeaineryhmä 
+CREATE TABLE drug_class ( -- yhdistyy vaikuttavaan aineeseen
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+
+--Vaikuttava aine
+CREATE TABLE active_ingredient (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+
+    drug_class_id INTEGER NOT NULL,
+    FOREIGN KEY (drug_class_id) REFERENCES drug_class(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE medicine_active_ingredient (
+    medicine_id INTEGER NOT NULL,
+    active_ingredient_id INTEGER NOT NULL,
+
+    PRIMARY KEY (medicine_id, active_ingredient_id),
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (active_ingredient_id) REFERENCES active_ingredient(id) ON DELETE RESTRICT
+);
+-- ---------------------------------------------------------------------
 
 /*
 * <-- usea vaihtoehto
