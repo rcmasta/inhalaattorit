535f042c547e6b869d493602ac302abe28931fa7
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-02-10 11:55:43 +0200

    add empty admin db function declarations

---
+++ backend/src/models/admin.model.js
@@ -0,0 +1,30 @@
+const db = require('../config/db');
+
+const dbAdd = () => {
+    try {
+        const query = "";
+        
+    } catch (err) {
+        throw err;
+    }
+};
+
+const dbEdit = () => {
+    try {
+        const query = "";
+        
+    } catch (err) {
+        throw err;
+    }
+};
+
+const dbRemove = () => {
+    try {
+        const query = "";
+        
+    } catch (err) {
+        throw err;
+    }
+};
+
+module.exports = { dbAdd, dbEdit, dbRemove }
