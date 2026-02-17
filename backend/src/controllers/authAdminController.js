3a58e3f6f45bcbc1681186b43fa620f4cfa7bd88
dfroji <dfroji@proton.me>
2026-02-17 12:26:20 +0200

    authAdminController.postLogin now reads admins from admins.json & added a test admin in admins.json

--- backend/src/controllers/authAdminController.js
+++ backend/src/controllers/authAdminController.js
@@ -2,50 +2,60 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('file-system');
 
-// TODO: get admins from db
-// const admins = require('');
+// assuming there will be only one or maybe even two admins,
+// having them in a json file should be sufficient.
+const ADMINS_DATA = 'data/admins.json';
 
 const INVALID_CREDENTIALS = 'Invalid credentials.';
 const ERROR = 'An error occurred.';
 
 class authAdminController {
     static postLogin = async (req, res) => {
-        // get email and password from request.
-        const {email, password} = req.body;
+        // get username and password from request.
+        const {username, password} = req.body;
 
-        // check if admin with given email exists
-        const admin = admins.find(a => a.email === email);
-        if (!admin) return res.status(401).json({message: INVALID_CREDENTIALS});
+        // read admins from file
+        await fs.readFile(ADMINS_DATA, "utf-8", async (err, data) => {
+            if (err) return res.status(500).json({message: ERROR});
 
-        // match the hash of the given password to the stored hash
-        try {
-            if (!await bcrypt.compare(password, admin.password)) {
-                return res.status(401).json({message: INVALID_CREDENTIALS});
+            const admins = JSON.parse(data);
+
+            // check if admin with given username exists
+            const admin = admins.find(a => a.username === username);
+            if (!admin) return res.status(401).json({message: INVALID_CREDENTIALS});
+
+            // match the hash of the given password to the stored hash
+            try {
+                if (!await bcrypt.compare(password, admin.password)) {
+                    return res.status(401).json({message: INVALID_CREDENTIALS});
+
+                }
+
+            } catch (error) {
+                return res.status(500).json({message: ERROR});
 
             }
 
-        } catch (error) {
-            return res.status(500).json({message: ERROR});
+            // sign and send token
+            try {
+                // read the private key for signing tokens
+                await fs.readFile('jwtPrivateKey.pem', 'utf-8', (err, data) => {
+                    if (err) {
+                        return res.status(500).json({message: ERROR});
+                    }
 
-        }
+                    // sign the token
+                    const token = jwt.sign({email: admin.email}, data, {algorithm: 'RS256', expiresIn: '1h'});
+                    return res.status(200).json({token, message: 'Authenticated successfully.'});
 
-        // sign and send token
-        try {
-            // read the private key for signing tokens
-            await fs.readFile('jwtPrivateKey.pem', 'utf-8', (err, data) => {
-                if (err) {
-                    return res.status(500).json({message: ERROR});
-                }
+                });
 
-                // sign the token
-                const token = jwt.sign({email: admin.email}, data, {algorithm: 'RS256', expiresIn: '1h'});
-                return res.status(200).json({token, message: 'Authenticated successfully.'});
+            } catch (err) {
+                return res.status(500).json({message: ERROR});
+            }
 
-            });
 
-        } catch (err) {
-            return res.status(500).json({message: ERROR});
-        }
+        });
     };
 }
 
