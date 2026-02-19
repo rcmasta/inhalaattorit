bede86a8f11f04f771ed28cc372518510af7a089
rcmasta <90792491+rcmasta@users.noreply.github.com>
2026-02-19 12:08:50 +0200

    Merge Frontend/main (#15)
    * Merge Frontend/skeleton (#4)
    
    * Chore: add git ignore
    
    * Feat: Frontend index page skeleton
    
    * Feat: Style index page to match mockup
    
    * Frontend skeleton: search page, admin panel, info and feedback pages, mobile support
    
    ---------
    
    Co-authored-by: Jarkko Lindfors <jarkko.lindfors@tuni.fi>
    Co-authored-by: Heikki Saxén <heikki.saxen@tuni.fi>
    
    * Merge Frontend/filters (#5)
    
    * Chore: add git ignore
    
    * Feat: Frontend index page skeleton
    
    * Feat: Style index page to match mockup
    
    * Frontend skeleton: search page, admin panel, info and feedback pages, mobile support
    
    * Add inhaler object filtering
    
    ---------
    
    Co-authored-by: Jarkko Lindfors <jarkko.lindfors@tuni.fi>
    Co-authored-by: Heikki Saxén <heikki.saxen@tuni.fi>
    
    * Merge Frontend/skeleton api mockup (#8)
    
    * Chore: add git ignore
    
    * Feat: Frontend index page skeleton
    
    * Feat: Style index page to match mockup
    
    * Frontend skeleton: search page, admin panel, info and feedback pages, mobile support
    
    * Feat: api.js mockup for frontend side + admin
    
    ---------
    
    Co-authored-by: Jarkko Lindfors <jarkko.lindfors@tuni.fi>
    Co-authored-by: Heikki Saxén <heikki.saxen@tuni.fi>
    
    * Add frontend filtering functionality and tidy up JS (#9)
    
    * Add filtering functionality
    
    * Split main.js for readability
    
    * Tidy up Main and Admin JavaScript
    
    * Fixes for sp1 presentation
    
    ---------
    
    Co-authored-by: Jarkko Lindfors <jarkko.lindfors@tuni.fi>
    Co-authored-by: Heikki Saxén <heikki.saxen@tuni.fi>

---
+++ frontend/js/main.js
@@ -0,0 +1,80 @@
+import { applyFilter } from './filter.js';
+import { getInhalers } from './api.js';
+
+/**
+ * Gathers all selected filters in an object
+ * @returns Object containing category-value pairs
+ */
+function getFilterObject() {
+    const selects = document.querySelectorAll(".inhaler-filter");
+    const filters = {};
+
+    selects.forEach(select => {
+        const key = select.name;
+        const value = select.value;
+
+        filters[key] = value;
+    });
+
+    return filters;
+}
+
+/**
+ * Filter fetched inhalers
+ */
+function filterData() {
+    const filters = getFilterObject();
+
+    const filtered = applyFilter(inhalers, filters);
+
+    // DEBUG
+    console.log("Filters:");
+    console.log(filters);
+    console.log("Results:");
+    console.log(filtered);
+}
+
+const inhalers = await getInhalers();
+
+// DEBUG
+console.log("All inhalers:");
+console.log(inhalers);
+
+// Document event listeners
+document.addEventListener("DOMContentLoaded", () => {
+    const filterToggle = document.querySelector(".filter-toggle");
+    const filterSection = document.querySelector(".search-filter");
+    if (filterToggle && filterSection) {
+        filterToggle.addEventListener("click", () => {
+            if (window.innerWidth <= 768) {
+                filterSection.classList.toggle("collapsed");
+            }
+        });
+    }
+
+    // Name search
+    const clearBtn = document.querySelector(".btn-clear");
+    const searchInput = document.getElementById("inhaler-name");
+    if (clearBtn && searchInput) {
+        clearBtn.addEventListener("click", () => {
+            searchInput.value = "";
+            searchInput.focus();
+        });
+    }
+
+    // Drop-down filters
+    const dropDownFilters = document.querySelectorAll(".inhaler-filter");
+    if (dropDownFilters) {
+        dropDownFilters.forEach(dropdown => dropdown.addEventListener("change", filterData));
+    }
+
+    // Clear dropdown filters
+    const clearFiltersBtn = document.querySelector(".btn-clear-filters");
+    if (clearFiltersBtn) {
+        clearFiltersBtn.addEventListener("click", () => {
+            const selects = document.querySelectorAll(".search-filter select");
+            selects.forEach(s => s.selectedIndex = 0);
+            filterData();
+        });
+    }
+});
---
+++ frontend/js/render.js
@@ -0,0 +1,2 @@
+// TODO: render cards to .results-grid
+// data from getInhalers() in api.js
