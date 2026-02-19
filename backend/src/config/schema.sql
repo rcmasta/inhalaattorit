c2fb2a38d7b32b075eb0f6ea9c83df6c3f8edd3f
Juuso Toivanen <juuso.v.toivanen@tuni.fi>
2026-02-19 16:01:56 +0200

    CASCADE and RESTRICT added to schema

--- backend/src/config/schema.sql
+++ backend/src/config/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE active_ingredient (
     name TEXT NOT NULL UNIQUE,
 
     drug_class_id INTEGER NOT NULL,
-    FOREIGN KEY (drug_class_id) REFERENCES drug_class(id)
+    FOREIGN KEY (drug_class_id) REFERENCES drug_class(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE color (
@@ -35,7 +35,7 @@ CREATE TABLE medicine (
     symptomatic_medicine BOOLEAN,
 
     inhaler_brand_id INTEGER,
-    FOREIGN KEY (inhaler_brand_id) REFERENCES inhaler_brand(id)
+    FOREIGN KEY (inhaler_brand_id) REFERENCES inhaler_brand(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE inhaler_brand (
@@ -48,104 +48,22 @@ CREATE TABLE medicine_intake_style (
     medicine_id INTEGER,
     intake_style_id INTEGER,
     PRIMARY KEY (medicine_id, intake_style_id),
-    FOREIGN KEY (medicine_id) REFERENCES medicine(id),
-    FOREIGN KEY (intake_style_id) REFERENCES intake_style(id)
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (intake_style_id) REFERENCES intake_style(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE medicine_active_ingredient (
     medicine_id INTEGER,
     active_ingredient_id INTEGER,
     PRIMARY KEY (medicine_id, active_ingredient_id),
-    FOREIGN KEY (medicine_id) REFERENCES medicine(id),
-    FOREIGN KEY (active_ingredient_id) REFERENCES active_ingredient(id)
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (active_ingredient_id) REFERENCES active_ingredient(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE medicine_color (
     medicine_id INTEGER,
     color_id INTEGER,
     PRIMARY KEY (medicine_id, color_id),
-    FOREIGN KEY (medicine_id) REFERENCES medicine(id),
-    FOREIGN KEY (color_id) REFERENCES color(id)
+    FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
+    FOREIGN KEY (color_id) REFERENCES color(id) ON DELETE RESTRICT
 );
--- ---------------------------------------------------------------------
-
-/*
-* <-- usea vaihtoehto
-Lääke
-    nimi
-    id?
-    image_path
-    kuvaus?
-Lääkemuoto*
-    jauhe
-    jauhekapseli
-    suihke
-    suihke sisäänhengityksen laukaisema
-    tilanjatkeella
-Ikä
-    Virallinen ikäraja (yli X vuosi) (lisätietona lapsiin)
-    lapset < 7v
-    Lapset 7-12v
-    Lapset >12v
-    Aikuinen > 18v
-Annostelu
-    Kerran päivässä
-    Kaksi kertaa päivässä
-    1x/pv ylläpidossa astma
-Sisäänhengitysnopeus ja koordinaatio
-    Sisäänhengitysnopeus hyvä (> 30l/min), hyvä koordinaatio
-    Sisäänhengitysnopeus huono (< 30l/min), hyvä koordinaatio
-    Sisäänhengitysnopeus hyvä (> 30l/min), huono koordinaatio
-    Sisäänhengitysnopeus huono (< 30 l/min), Huono koordinaatio
-Inhalaattori*
-    Ellipta
-    DIskus
-    Trubuhaler
-    Easyhaler
-    Nexthaler
-    Twisthaler
-    Breezhaler
-    Respimat
-    Evohaler
-    Autohaler
-    Genuair
-    Aerosphere
-Lääkken käyttötarkoitus*
-    Hoitava lääke
-    Oirelääke
-Lääkeaineryhmä*
-    ICS (hengitettävä kortisoni, inhaled corticosteroids)
-    LAMA (pitkävaikutteinen antikolinergi, long acting muscarinic antagonist)
-    LABA (pitkävaikutteinen β2-agonisti, long acting β2-agonist)
-    SABA (lyhytvaikutteinen β2-agonisti, short acting β2-agonist)
-    SAMA (lyhytvaikutteinen antikolinergi, short acting muscarinic antagonist)
-Vaikuttava lääkeaine*
-    Beklometasoni ICS
-    Budesonidi ICS
-    Flutikasonipropionaatti ICS
-    Flutikasonifuroaatti ICS
-    Mometasoni ICS
-    Siklesonidi ICS
-    Formoteroli LABA
-    Salmeteroli LABA
-    Vilanteroli LABA
-    Indakateroli LABA
-    Olodateroli LABA
-    Tiotropiumbromidi LAMA
-    Aklidiniumbromidi LAMA
-    Glykopyrroniumbromidi LAMA
-    Umeklinidiumbromidi LAMA
-    Salbutamoli SABA
-    Terbutaliini SABA
-Väri
-    Valkoinen
-    Pinkki
-    Punainen
-    Keltainen
-    Oranssi
-    Violetti
-    Sininen
-    Harmaa
-    Ruskea
-    Vihreaä
-*/
