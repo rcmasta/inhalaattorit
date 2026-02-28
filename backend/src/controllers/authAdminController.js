075196780423611af5d709c33fa98d55d3a54fe3
froji <dfroji@proton.me>
2026-02-28 14:57:16 +0200

    refactored authAdminController to a controller and a model (#20)

--- backend/src/controllers/authAdminController.js
+++ backend/src/controllers/authAdminController.js
@@ -1,11 +1,5 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const fs = require('file-system');
+const {getAdmin, verifyPassword, signToken} = require('../models/auth.model');
 const BackendError = require('../classes/backendError');
-
-// assuming there will be only one or maybe even two admins,
-// having them in a json file should be sufficient.
-const ADMINS_DATA = 'data/admins.json';
 
 const INVALID_CREDENTIALS = 'Invalid credentials.';
 
@@ -14,36 +8,18 @@ class authAdminController {
         // get username and password from request.
         const {username, password} = req.body;
 
-        // read admins from file
-        await fs.readFile(ADMINS_DATA, "utf-8", async (err, data) => {
+        // get admin with the given username,
+        // if admin is null the given credentials are invalid
+        const admin = await getAdmin(username);
+        if (!admin) return next(new BackendError(401, INVALID_CREDENTIALS));
+
+        // validate the given password of the valid admin
+        if (!await verifyPassword(admin, password)) return next(new BackendError(401, INVALID_CREDENTIALS));
+
+        // sign and return a jwt
+        await signToken(admin, (err, token) => {
             if (err) return next(err);
-
-            const admins = JSON.parse(data);
-
-            // get admin from admins
-            const admin = admins.find(a => a.username === username);
-            try {
-                // check if admin with given username exists
-                if (!admin) throw new BackendError(401, INVALID_CREDENTIALS);
-
-                // match the hash of the given password to the stored hash
-                if (!await bcrypt.compare(password, admin.password)) {
-                    throw new BackendError(401, INVALID_CREDENTIALS);
-                }
-
-            } catch (err) {
-                return next(err);
-            }
-
-            // read the private key for signing tokens
-            await fs.readFile('jwtPrivateKey.pem', 'utf-8', (err, data) => {
-                if (err) return next(err);
-
-                // sign the token
-                const token = jwt.sign({email: admin.email}, data, {algorithm: 'RS256', expiresIn: '1h'});
-                return res.status(200).json({token, message: 'Authenticated successfully.'});
-
-            });
+            return res.status(200).json({token, message: 'Authenticated successfully.'});
         });
     };
 }
