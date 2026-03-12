7985da61743796590451e6d33e2c1a0d8b3160de
dfroji <dfroji@proton.me>
2026-03-12 11:32:23 +0200

    inhalers controller and model as well as admin controller and model now utilize the error middleware

--- backend/src/controllers/inhalersController.js
+++ backend/src/controllers/inhalersController.js
@@ -4,16 +4,12 @@ const InhalersService = require('../services/inhalersService');
 class InhalersController {
 
     static getInhalers(req, res) {
-        try {
-            let lang = req.query.lang;
-            if (lang !== "fi" && lang !== "sv" ) {
-                lang = "fi";
-            }
-            const data = InhalersService.getAllInhalers(lang);
-            res.json(data);
-        } catch (e) {
-            res.status(500).json({message: e.message});
+        let lang = req.query.lang;
+        if (lang !== "fi" && lang !== "sv" ) {
+            lang = "fi";
         }
+        const data = InhalersService.getAllInhalers(lang);
+        res.json(data);
     }
 }
 
