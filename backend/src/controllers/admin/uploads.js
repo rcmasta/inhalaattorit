ab44350d93ff55d46cdec64ad930d94b5c00fc58
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-03-19 20:07:32 +0200

    image uploading now replaces medicines old picture with new one and possibility to remove images

--- backend/src/controllers/admin/uploads.js
+++ backend/src/controllers/admin/uploads.js
@@ -1,44 +1,63 @@
 const adminModel = require('../../models/admin/adminModel');
-const BackendError = require('../../classes/backendError');
-const dbInhalers = require ('../../models/admin/inhalers');
 
 const sharp = require('sharp')
 const path = require('path')
-
-const fullResSize = 2048;
-const thumbResSize = 512;
+const fs = require('fs/promises')
 
 class uploads {
     static add = async (req, res) => {
         try {
             const itemId = req.params.id;
             const file = req.file;
-            const filename = Date.now() - Math.random() + '.jpeg';
 
-            const fullResPath = path.join('uploads', 'full', filename);
-            const thumbResPath = path.join('uploads', 'thumb', filename);
+            if (!file) {
+                return res.status(400).json({message: "Image not given"});
+            }
 
+            const metadata = await sharp(file.buffer).metadata();
+
+            let fullSize = metadata.width >= metadata.height ? metadata.height : metadata.width;
+            const thumbSize = fullSize > 500 ? 500 : fullSize;
+            if (fullSize > 2160) { fullSize = 2160; } // max size is standard 4K 
+
+            const fullResFolder = path.join(__dirname, '../../../uploads/full');
+            const thumbResFolder = path.join(__dirname, '../../../uploads/thumb');
+
+            // creates missing folders
+            await fs.mkdir(fullResFolder, { recursive: true });
+            await fs.mkdir(thumbResFolder, { recursive: true });
+
+            // full size picture
             await sharp(file.buffer)
-                .resize(fullResSize)
+                .resize({ width: fullSize, height: fullSize })
                 .jpeg({quality: 90})
-                .toFile(fullResPath);
+                .toFile(path.join(fullResFolder, itemId + ".jpeg"));
 
+            // thumbnail picture
             await sharp(file.buffer)
-                .resize(thumbResSize)
+                .resize({ width: thumbSize, height: thumbSize})
                 .jpeg({quality: 50})
-                .toFile(thumbResPath);
-
-            dbInhalers.edit(itemId, { image_path: filename });
-
+                .toFile(path.join(thumbResFolder, itemId + ".jpeg"));
+     
             res.status(201).json({message: 'Image added successfully'});
         } catch (err) {
             res.status(400).json({message: 'Image was not added'});
         }
     };
 
-    static delete = (req, res) => {
-        const id = parseInt(req.params.id);
-        const filename = "";
+    static delete = async (req, res) => {
+
+        const itemId = req.params.id;
+        const fullResImage = path.join(__dirname, '../../../uploads/full/' + itemId + ".jpeg");
+        const thumbResImage = path.join(__dirname, '../../../uploads/thumb/' + itemId + ".jpeg");
+
+        try { 
+            await fs.unlink(fullResImage); 
+            await fs.unlink(thumbResImage);
+            res.status(200).json({message: 'Image removed successfully'});
+        } catch {
+            res.status(400).json({message: "Image not removed"});
+        }
     };
 };
 
