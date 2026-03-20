e717e579960dc9509997eb56548a600f93409993
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-03-20 13:51:27 +0200

    refactoring of uploads.js

--- backend/src/controllers/admin/uploads.js
+++ backend/src/controllers/admin/uploads.js
@@ -1,10 +1,11 @@
 const adminModel = require('../../models/admin/adminModel');
 const db = require('../../config/db');
-
-
 const sharp = require('sharp')
 const path = require('path')
 const fs = require('fs/promises')
+
+const MAX_FULL_RES = 2160; // limits full size res 
+const MAX_THUMB_RES = 500; // limits thumbnail res
 
 class uploads {
     static add = async (req, res) => {
@@ -20,14 +21,13 @@ class uploads {
 
             const metadata = await sharp(file.buffer).metadata();
 
-            let fullSize = metadata.width >= metadata.height ? metadata.height : metadata.width;
-            const thumbSize = fullSize > 500 ? 500 : fullSize;
-            if (fullSize > 2160) { fullSize = 2160; } // max size is standard 4K 
+            const fullSize = Math.min(metadata.width, metadata.height, MAX_FULL_RES);
+            const thumbSize = Math.min(fullSize, MAX_THUMB_RES);
 
             const fullResFolder = path.join(__dirname, '../../../uploads/full');
             const thumbResFolder = path.join(__dirname, '../../../uploads/thumb');
 
-            // creates missing folders
+            // creates if missing folders
             await fs.mkdir(fullResFolder, { recursive: true });
             await fs.mkdir(thumbResFolder, { recursive: true });
 
@@ -45,6 +45,7 @@ class uploads {
      
             res.status(201).json({message: 'Image added successfully'});
         } catch (err) {
+            console.log(err);
             res.status(400).json({message: 'Image was not added'});
         }
     };
@@ -52,10 +53,12 @@ class uploads {
     static delete = async (req, res) => {
 
         const itemId = req.params.id;
+        if (!itemId) { return res.status(400).json({message: "ID parameter missing"}); }
+
         if (await this.deleteWithId(itemId)) {
             res.status(200).json({message: 'Image removed successfully'});
         } else {
-            res.status(400).json({message: "Image not removed"});
+            res.status(404).json({message: "Image not found"});
         }
     };
 
@@ -69,7 +72,6 @@ class uploads {
         } catch {
             return false;
         }
-
         return true;
     };
 };
