99dac4262bc48c932d7b3afe64109f66c93e5e97
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-03-25 13:17:30 +0200

    admin can now request non trivial fields for filters

--- backend/src/controllers/admin/inhalers.js
+++ backend/src/controllers/admin/inhalers.js
@@ -1,5 +1,6 @@
 const adminModel = require('../../models/admin/adminModel');
 const BackendError = require('../../classes/backendError');
+const db = require('../../config/db');
 
 class inhalers {
     static create = (req, res, next) => {
@@ -35,6 +36,43 @@ class inhalers {
         res.status(200).json({message: 'Item removed successfully'});
     };
 
+    static filters = (req, res, next) => {
+
+        const intakeMap = getAllLangFromDB("intake_style");
+        const ingredientsMap = getAllLangFromDB("active_ingredient");
+        const colorsMap = getAllLangFromDB("color");
+        const brandMap = Object.fromEntries(db.prepare(`SELECT id, name FROM inhaler_brand`).all()
+                         .map(r => [r.id, {id: r.id, name: r.name}]));
+
+        res.status(200).json({ 
+            active_ingredients: Object.values(ingredientsMap),
+            colors: Object.values(colorsMap),
+            intake_styles: Object.values(intakeMap),
+            inhaler_brands: Object.values(brandMap) 
+        });
+    };
+};
+
+const allowedValues = ["intake_style", "active_ingredient", "color"];
+const getAllLangFromDB = (search) => {
+
+    if (!allowedValues.includes(search)){
+        return {};
+    }
+
+    const rows = db.prepare(
+        `SELECT main.id, lang.language, lang.name
+         FROM ${search} AS main
+         LEFT JOIN ${search}_translation AS lang
+         ON main.id = lang.${search}_id`).all();
+
+    const res = {};
+    rows.forEach(r => {
+        if (!res[r.id]) { res[r.id] = { id: r.id, name: {} }; }
+        if (r.language) { res[r.id].name[r.language] = r.name; }
+    });
+
+    return res;
 };
 
 module.exports = inhalers;
