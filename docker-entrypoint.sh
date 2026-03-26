3534e30332d55140ed11e5402628f086e858f8ba
rcmasta <90792491+rcmasta@users.noreply.github.com>
2026-03-26 13:40:53 +0200

    Add default data in case volume data folder is empty (#42)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ ! -f /backend/data/.initialized ]; then
+  echo "Seeding persistent volume with initial data."
+  cp -R /backend/default_data/* /backend/data/
+  touch /backend/data/.initialized
+fi
+
 echo "Checking test database"
 
 if [ ! -f src/config/inhalers.db ]; then
-    echo "Database not found. Generating..."
+    echo "Database not found. Generating."
     npm run test_db
 else
     echo "Database already exists. Skipping generation."
