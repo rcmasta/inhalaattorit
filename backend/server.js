152d27ad48c6109deffd9b4b56c331a3d174c0f3
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-04-01 22:58:47 +0300

    automatically creates needed folders for image uploading

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
+const initFolders = require('./src/services/fileSystemService');
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
+    initFolders();
     console.log(`Server running on port ${PORT}`);
 });
--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -4,9 +4,9 @@ const { randomUUID } = require("crypto");
 
 // saves user given image as tmp file to disk
 const storage = multer.diskStorage({
-    // destination is in data/tmp/
+    // destination is in uploads/tmp/
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../data/tmp"));
+        cb(null, path.join(__dirname, "../../data/uploads/tmp"));
     },
     // unique name even if 2 uploads at the same time with same name
     filename: (req, file, cb) => {
