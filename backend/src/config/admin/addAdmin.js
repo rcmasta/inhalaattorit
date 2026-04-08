aac4e7a75e0a9b6f9bf1fa68c4024c98af083e8e
dfroji <dfroji@proton.me>
2026-04-08 19:16:40 +0300

    implemented script for adding a new admin

---
+++ backend/src/config/admin/addAdmin.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const genPassword = require('./genPassword');
+
+let admins = require('../../../data/admindata/admins.json');
+
+const username = process.argv[2]
+const passwordLen = process.argv[3];
+
+// generate cryptographically secure random password
+genPassword(passwordLen, (err, pw) => {
+    if (err) throw err;
+
+    // salt and hash the generated password
+    bcrypt.hash(pw, 10, (err, hash) => {
+        if (err) throw err;
+
+        admins.push({
+                     "username":username, 
+                     "password":hash
+        });
+
+        // write the new admin to admins.json
+        fs.writeFileSync('./data/admindata/admins.json', JSON.stringify(admins, null, 2));
+        console.log(`Successfully generated admin '${username}' with password '${pw}'`);
+    })
+})
