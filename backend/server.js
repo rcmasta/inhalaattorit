00eac5f38dd1f79d172739e46a4cbfd35a84c7c0
vilz0 <256529093+vilz0@users.noreply.github.com>
2026-04-10 19:50:48 +0300

    reformating code

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-const serverInit = require('./src/init/serverInit');
+const bootstrapApp = require('./src/bootstrap/index');
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        await serverInit();
+        await bootstrapApp();
         const app = require('./app');
 
         app.listen(PORT, "0.0.0.0", () => {
+            console.log('Application started successfully');
             console.log(`Server running on port ${PORT}`);
         });
     } catch(err) {
-        console.log("Startup failed: ", err);
+        console.log("Server startup failed: ", err);
         process.exit(1);
     }
 };
