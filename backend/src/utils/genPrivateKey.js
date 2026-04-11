09c5062200a0d7f288de27e19ff0e2ed95348606
froji <dfroji@proton.me>
2026-04-11 11:04:35 +0300

    refactored initialization of jwt key to use fs promises and added a simple check to genPrivateKey.js

--- backend/src/utils/genPrivateKey.js
+++ backend/src/utils/genPrivateKey.js
@@ -3,8 +3,7 @@ const openssl = require('openssl-nodejs');
 function genPrivateKey(callback) {
     openssl('genrsa 2048', (err, buffer) => {
         // apparently the err here is not useful due to how the library is made lol...
-
-        // pass the generated key as a string
+        if (!buffer) return callback(Error('Failed to generate a key.'), null);
         callback(null, buffer.toString());
     });
 };
